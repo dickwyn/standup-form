01c266bee7fac44634e4b92590e5e279b71b0d90
dickwyn <dickwyn@outlook.com>
2020-04-25 00:21:27 -0700

    (config) Update app name to standup-form

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# StandupGenerator
+# StandupForm
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'standup-generator'`, () => {
+  it(`should have as title 'standup-form'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('standup-generator');
+    expect(app.title).toEqual('standup-form');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('standup-generator app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'standup-form app is running!'
+    );
   });
 });
