e36796a081e18d180eca06101d88e65b65751a73
dickwyn <dickwyn@outlook.com>
2020-04-24 17:40:18 -0700

    (feat) Create initial form layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'standup-generator';
+export class AppComponent implements OnInit {
+  @Input() appearance: 'outline';
+  standupForm: FormGroup;
+  date: String;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    const todayDateObj = new Date();
+    const month = todayDateObj.getMonth();
+    const day = todayDateObj.getDate();
+    const year = String(todayDateObj.getFullYear()).slice(-2);
+    this.date = `${month}/${day}/${year}`;
+
+    this.standupForm = this.fb.group({
+      previousChecklist: this.fb.array(['']),
+      currentChecklist: this.fb.array(['']),
+      blockers: '',
+    });
+
+    // for debugging purposes only
+    this.standupForm.valueChanges.subscribe(console.log);
+  }
+
+  get currentChecklist() {
+    return this.standupForm.get('currentChecklist') as FormArray;
+  }
+
+  get previousChecklist() {
+    return this.standupForm.get('previousChecklist') as FormArray;
+  }
+
+  addToChecklist(controlName: string) {
+    const formControl = this.standupForm.get(controlName) as FormArray;
+    formControl.push(new FormControl(''));
+  }
+
+  removeFromChecklist(controlName: string, index: number) {
+    const formControl = this.standupForm.get(controlName) as FormArray;
+    formControl.removeAt(index);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
