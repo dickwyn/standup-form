b296e2a47198b8957ee6eef6c7f28174f5c18d4f
dickwyn <dickwyn@outlook.com>
2020-05-05 23:21:56 -0700

    (feat) Save and auto-populate previous day's checklist

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormArray } from '@angular/forms';
+import { FormArray, FormControl } from '@angular/forms';
 import ClipboardJS from 'clipboard';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -29,7 +29,30 @@ export class PreviewComponent implements OnInit {
     this.isMonday = todayDateObj.getDay() === 1;
     this.onWeekdayRegistered.emit(this.isMonday);
 
+    if (localStorage.getItem('STANDUP_FORM_DATA') !== null) {
+      const restored = JSON.parse(localStorage.getItem('STANDUP_FORM_DATA'));
+
+      if (restored.expiration < todayDateObj.getTime()) {
+        restored.currentChecklist.forEach((item, index) => {
+          this.currentChecklist.setControl(index, new FormControl(item));
+        });
+        restored.previousChecklist.forEach((item, index) => {
+          this.previousChecklist.setControl(index, new FormControl(item));
+        });
+      } else {
+        restored.currentChecklist.forEach((item, index) => {
+          this.previousChecklist.setControl(index, new FormControl(item));
+        });
+      }
+    }
+
     this.clipboardjs.on('success', () => {
+      const dataToSave = {
+        expiration: todayDateObj.setHours(24, 0, 0, 0),
+        currentChecklist: this.currentChecklist.value,
+        previousChecklist: this.previousChecklist.value,
+      };
+      localStorage.setItem('STANDUP_FORM_DATA', JSON.stringify(dataToSave));
       this.snackbar.open('Text copied', '', {
         duration: 3000,
         panelClass: 'copy-success',
