4b72b4b50f39524c3ddb94cc89b5a571a8e80b18
dickwyn <dickwyn@outlook.com>
2020-05-18 21:36:11 -0700

    (feat) Add github link and version info on footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import newGithubIssueUrl from 'new-github-issue-url';
+import { name, version, repository } from 'package.json';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  version = version;
+  name = name;
+  repositoryUrl = repository.url;
+  newIssueUrl;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.newIssueUrl = newGithubIssueUrl({
+      user: 'dickwyn',
+      repo: name,
+      body: '[Enter feedback here]',
+    });
+  }
 }
